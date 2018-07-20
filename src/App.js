7afd2a10d0250503a72eb7b280fa577f416b2192
Zsemle <zsemlerider@gmail.com>
2018-07-20 15:52:55 +0200

    last save before eject

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+import './Person/Person.css';
+
+class App extends Component {
+  state = {
+    persons: [
+      {id: 'srygdf3fffffsd',name: 'Max',age: 28},
+      {id: 'dfgmnjklmdfgff',name: 'Imre',age: 32},
+      {id: 'rtsr54z754zhrt',name: 'Stephanie',age: 26},
+    ],
+    otherState: {
+      some: 'other state'
+    },
+    showPersons: false
+  }
+
+  deletePersonHandler = (personIndex) =>{
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  nameChangedHandler = (event,id) => {
+    const personIndex = this.state.persons.findIndex(p => p.id === id);
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  render() {
+    const style = {
+      backgroundColor: "green",
+      color: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    }
+
+    let classes = [];
+    if(this.state.persons.length<=2){
+      classes.push("red")
+    }
+    if(this.state.persons.length<=1){
+      classes.push("red","bold")
+    }
+
+    let persons = null;
+
+    if (this.state.showPersons){
+      persons = (
+        <div>
+          {this.state.persons.map((person,index) => {
+            return <Person
+              name={person.name}
+              age={person.age}
+              click={()=>this.deletePersonHandler(index)}
+              changed={(event)=>this.nameChangedHandler(event,person.id)}
+              key={person.id} />
+          })}
+        </div>
+      )
+      style.backgroundColor = "red";
+    }
+
+    return (
+        <div className="App">
+          <h1>Hi, I'm a react app</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button style={style} onClick={() => this.togglePersonsHandler()}>Toggle Persons</button>
+          {persons}
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const person = (props) => {
+  const style = {
+
+    }
+
+  return (
+    <div className="Person" style={style}>
+      <p onClick={props.click}>I'm {props.name}, and I am  {props.age} years old.</p>
+      <p>{props.children}</p>
+      <p><input type="text" onChange={props.changed} value={props.name}/></p>
+    </div>
+  )
+};
+
+export default person;
