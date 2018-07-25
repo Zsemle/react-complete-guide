68a5a6a35dd3b430b06647a43ca1e104dfc5143b
Zsemle <zsemlerider@gmail.com>
2018-07-25 18:30:00 +0200

    returning an array of elements, instead of a single root element

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -14,13 +14,16 @@ class Person extends Component{
   }
   render(){
     console.log('[Person.js] inside render.')
-    return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>I'm {this.props.name}, and I am  {this.props.age} years old.</p>
-        <p>{this.props.children}</p>
-        <p><input type="text" onChange={this.props.changed} value={this.props.name}/></p>
-      </div>
-    )
+    // return (
+    //   <div className={classes.Person}>
+    //     <p onClick={this.props.click}>I'm {this.props.name}, and I am  {this.props.age} years old.</p>
+    //     <p>{this.props.children}</p>
+    //     <p><input type="text" onChange={this.props.changed} value={this.props.name}/></p>
+    //   </div>
+    // )
+    return [<p key="1" onClick={this.props.click}>I'm {this.props.name}, and I am  {this.props.age} years old.</p>,
+        <p key="2">{this.props.children}</p>,
+        <p key="3"><input type="text" onChange={this.props.changed} value={this.props.name}/></p>]
   }
 }
 
