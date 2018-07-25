9881c76b44443dd67ccec02b95bab1dd2bfeec57
Zsemle <zsemlerider@gmail.com>
2018-07-25 17:41:46 +0200

    lifecycle hooks for update

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,6 +14,16 @@ class App extends Component {
   componentDidMount(){
     console.log('[App.js] inside componentDidMount.')
   }
+  shouldComponentUpdate(nextProps,nextState){
+    console.log('[UPDATE App.js] inside shouldComponentUpdate',nextProps,nextState)
+    return true;
+  }
+  componentWillUpdate(nextProps,nextState){
+    console.log('[UPDATE App.js] inside componentWillUpdate',nextProps,nextState)
+  }
+  componentDidUpdate(){
+    console.log('[UPDATE App.js] inside componentDidUpdate')
+  }
 
   state = {
     persons: [
