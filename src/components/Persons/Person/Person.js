ad632bef79258e6721b095126275f18cf5027865
Zsemle <zsemlerider@gmail.com>
2018-07-27 09:24:56 +0200

    react's own reference

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -8,6 +8,7 @@ class Person extends Component{
   constructor(props){
     super(props);
     console.log('[Person.js] inside constructor.',props);
+    this.inputElement = React.createRef();
   }
   componentWillMount(){
     console.log('[Person.js] inside componentWillMount.')
@@ -15,7 +16,7 @@ class Person extends Component{
   componentDidMount(){
     console.log('[Person.js] inside componentDidMount.')
     if (this.props.position === 0){
-      this.inputElement.focus();
+      this.inputElement.current.focus();
     }
   }
   render(){
@@ -25,7 +26,7 @@ class Person extends Component{
         <p onClick={this.props.click}>I'm {this.props.name}, and I am  {this.props.age} years old.</p>
         <p>{this.props.children}</p>
         <p><input
-          ref={(inp)=>{this.inputElement = inp}}
+          ref={this.inputElement}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}/></p>
